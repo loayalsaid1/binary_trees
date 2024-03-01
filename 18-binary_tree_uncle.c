3251131fca2b9642e48017058fdd6fb2a94fd758
Loay Al-Said <l.programming555@gmail.com>
2024-03-01 20:07:34 +0200

    fix 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -14,5 +14,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent == NULL)
 		return (NULL);
 
-	return (node->parent->parent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
 }
