9a831139351d4c6a9fa6a1a55158d68270742b9f
Loay Al-Said <l.programming555@gmail.com>
2024-03-01 02:58:32 +0200

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 
+
+/**
+ * binary_tree_balance - Get the balance factor or a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: The difference between the max height of left
+ * subtree and right one
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int  left_max_height, right_max_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_max_height = binary_tree_height(tree->left);
+	right_max_height = binary_tree_height(tree->right);
+
+	return (left_max_height - right_max_height);
+}
+
 /**
  * binary_tree_height - Get the height of the tree
  * description: Get the max height of the tree execluding the root node
@@ -12,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
