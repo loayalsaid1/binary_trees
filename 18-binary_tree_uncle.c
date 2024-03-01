1f5325ab11c602d366af2466dc1895038b940abd
Loay Al-Said <l.programming555@gmail.com>
2024-03-01 04:47:33 +0200

    125-O

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_uncle - get the ungle
+ * @node: the node
+ * Return: node
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	return (node->parent->parent->left);
+}
